9dba04e2b43057bb36affa781202418e4b23e5b5
hi-imAndy <andrej.1998@uns.ac.rs>
2023-01-02 20:07:29 +0100

    load dataset & train simple model

---
+++ data/data_loader.py
@@ -0,0 +1,26 @@
+from keras.utils import image_dataset_from_directory
+
+all_levels = ['easy', 'medium_easy', 'medium_hard', 'hard']
+images_levels = ['easy']
+base_url = '../generated_images/dataset'
+IMG_SIZE = 96
+BATCH_SIZE = 100
+
+
+def load_dataset():
+    train, val = None, None
+    for level in all_levels:
+        train_ds = image_dataset_from_directory(
+            validation_split=0.2, subset="training", seed=123,
+            directory=base_url + '/' + level, labels='inferred', batch_size=BATCH_SIZE,
+            image_size=(IMG_SIZE, IMG_SIZE))
+        val_ds = image_dataset_from_directory(
+            validation_split=0.2, subset="validation", seed=123,
+            directory=base_url + '/' + level, labels='inferred', batch_size=BATCH_SIZE,
+            image_size=(IMG_SIZE, IMG_SIZE))
+        if train is None and val is None:
+            train, val = train_ds, val_ds
+        else:
+            train = train.concatenate(train_ds)
+            val = val.concatenate(val_ds)
+    return train, val
--- dnn/train.py
+++ dnn/train.py
@@ -1,2 +1,44 @@
+import tensorflow as tf
 
+import matplotlib.pyplot as plt
+from data.data_loader import load_dataset
+
+class_names = ['ellipse', 'square', 'triangle']
+
+
+def setup_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Rescaling(1. / 255),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(3)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    return model
+
+
+def plot_history(history):
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+
+def train_model():
+    model = setup_model()
+    train_ds, val_ds = load_dataset()
+    history = model.fit(train_ds, epochs=100, validation_data=val_ds)
+    plot_history(history)
+    return model, history
 
